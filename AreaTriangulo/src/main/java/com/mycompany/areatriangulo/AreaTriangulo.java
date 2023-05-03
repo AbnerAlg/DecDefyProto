baff12db0184ba2c7aed2e2ac35869c0cba89808
Abner Alain  Gil Garcia <124418451+AbnerAlg@users.noreply.github.com>
2023-05-03 16:48:01 -0600

    Se sube archivo

---
+++ AreaTriangulo/src/main/java/com/mycompany/areatriangulo/AreaTriangulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.areatriangulo;
+
+/**
+ *
+ * @author abner
+ */
+public class AreaTriangulo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
